7c086365d193f8faecb504cb264f21fdf38285d4
Naoto Takezawa <naoto9march@gmail.com>
2020-03-21 11:14:51 +0900

    ユニットテストを追加

--- src/lib.rs
+++ src/lib.rs
@@ -29,3 +29,38 @@ where
     let z = operation(x, y);
     stack.push(z);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn addition(){
+        let ans = calcurate("1.21 8.98 + ");
+        assert_eq!("10.19", format!("{:.2}", ans));
+    }
+
+    #[test]
+    fn subtraction(){
+        let ans = calcurate("1.21 8.98 - ");
+        assert_eq!("-7.77", format!("{:.2}", ans));
+    }
+
+    #[test]
+    fn multiplication(){
+        let ans = calcurate("1.21 8.98 * ");
+        assert_eq!("10.87", format!("{:.2}", ans));
+    }
+
+    #[test]
+    fn division(){
+        let ans = calcurate("1.21 8.98 / ");
+        assert_eq!("0.13", format!("{:.2}", ans));
+    }
+
+    #[test]
+    fn three_operands(){
+        let ans = calcurate("1.21 8.98 0.74 + - ");
+        assert_eq!("-8.51", format!("{:.2}", ans));
+    }
+}
